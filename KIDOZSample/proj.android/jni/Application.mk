969e9e70a6c5f2133a7d697a39819fc345372c1e
orikidoz <ori@kidoz.net>
2016-09-14 16:19:23 +0300

    remove old projects

--- KIDOZSample/proj.android/jni/Application.mk
+++
@@ -1,3 +0,0 @@
-APP_STL := gnustl_static
-APP_CPPFLAGS := -frtti -DCOCOS2D_DEBUG=1
-APP_ABI := armeabi armeabi-v7a
--- KIDOZSample/proj.android/obj/local/armeabi/objs/cocos2dx_static/ccFPSImages.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs/cocos2dx_static/ccFPSImages.o: \
- /cygdrive/c/cocos2d-x-2.2.6/cocos2dx/ccFPSImages.c \
- /cygdrive/c/cocos2d-x-2.2.6/cocos2dx/ccFPSImages.h
-
-/cygdrive/c/cocos2d-x-2.2.6/cocos2dx/ccFPSImages.h:
